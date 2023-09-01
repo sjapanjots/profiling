2d2caefa1d7b120047ca161df759027bce5ba1ca
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-02 03:30:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from operator import index
 import streamlit as st
-from pycaret.regression import setup, compare_models, pull, save_model, load_model
 import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
@@ -29,15 +28,7 @@ if choice == "Profiling":
     st_profile_report(profile_df)
 
 if choice == "Modelling": 
-    chosen_target = st.selectbox('Choose the Target Column', df.columns)
-    if st.button('Run Modelling'): 
-        setup(df, target=chosen_target, silent=True)
-        setup_df = pull()
-        st.dataframe(setup_df)
-        best_model = compare_models()
-        compare_df = pull()
-        st.dataframe(compare_df)
-        save_model(best_model, 'best_model')
+    pass
 
 if choice == "Download": 
     with open('best_model.pkl', 'rb') as f: 
