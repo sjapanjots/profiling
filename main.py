e427b09dbbaafc141164982e5983d1322de809e2
Japanjot Singh <123179832+sjapanjots@users.noreply.github.com>
2023-09-02 02:57:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from operator import index
 import streamlit as st
-import plotly.express as px
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
 import pandas_profiling
 import pandas as pd
